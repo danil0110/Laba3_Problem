b6bc487893ebd00988fe4c3eb1f6147012661804
danil0110 <danilmail0110@gmail.com>
2020-03-23 20:19:55 +0200

    Repo created

---
+++ Laba3_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laba3_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
