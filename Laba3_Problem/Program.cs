f4aa093b1d0e79df1a28908f176d5ce05cf051e3
danil0110 <danilmail0110@gmail.com>
2020-03-24 15:23:01 +0200

    Finished working calculator code

--- Laba3_Problem/Program.cs
+++ Laba3_Problem/Program.cs
@@ -8,6 +8,7 @@ namespace Laba3_Problem
     {
         static void Main(string[] args)
         {
+/*
             string problem = "";
             if (args.Length > 0)
                 foreach (var el in args)
@@ -18,19 +19,58 @@ namespace Laba3_Problem
                             problem += symbol;
                     }
                 }
+*/
 
+            string problem = "-23-2*9/4*2-11";
             string[] result = Parse(problem);
-            MyStack<int> numbers = new MyStack<int>();
+            MyStack<float> numbers = new MyStack<float>();
             MyStack<string> symbols = new MyStack<string>();
             foreach (var el in result)
             {
-                int number;
-                bool success = int.TryParse(el, out number);
+                float number;
+                bool success = float.TryParse(el, out number);
                 if (success)
                     numbers.Push(number);
-                else
+                else if (symbols.IsEmpty)
                     symbols.Push(el);
+                else if (el == "*" || el == "/")
+                {
+                    if (symbols.Peek() == "+" || symbols.Peek() == "-")
+                        symbols.Push(el);
+                    else
+                        while (!symbols.IsEmpty && symbols.Peek() != "+" && symbols.Peek() != "-")
+                        {
+                            Calculate(numbers, symbols);
+                            if (symbols.IsEmpty)
+                            {
+                                symbols.Push(el);
+                                break;
+                            }
+                            else if (symbols.Peek() == "+" || symbols.Peek() == "-")
+                            {
+                                symbols.Push(el);
+                                break;
+                            }
+                        }
+                }
+                else
+                    while (!symbols.IsEmpty)
+                    {
+                        Calculate(numbers, symbols);
+                        if (symbols.IsEmpty)
+                        {
+                            symbols.Push(el);
+                            break;
+                        }
+                    }
+                
             }
+
+            while (!symbols.IsEmpty)
+            {
+                Calculate(numbers, symbols);
+            }
+            Console.WriteLine($"Result: {numbers.Pop()}");
             
         }
 
@@ -63,9 +103,34 @@ namespace Laba3_Problem
                 i = pos2 + 1;
             }
             result.RemoveAt(result.Count - 1);
-
+            if (result[0] == "-")
+            {
+                result.RemoveAt(0);
+                result[0] = "-" + result[0];
+            }
             return result.ToArray();
         }
-        
+
+        public static void Calculate(MyStack<float> numbers, MyStack<string> symbols)
+        {
+            float b = numbers.Pop();
+            float a = numbers.Pop();
+            string sym = symbols.Pop();
+            float result = 0;
+            switch (sym)
+            {
+                case "+":
+                    result = a + b;
+                break;
+                case "-": result = a - b;
+                    break;
+                case "*": result = a * b;
+                    break;
+                case "/": result = a / b;
+                    break;
+            }
+            numbers.Push(result);
+        }
+
     }
 }
