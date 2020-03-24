465c283ca47a185f55d57bf6406805c6a061b3e6
danil0110 <danilmail0110@gmail.com>
2020-03-24 13:43:04 +0200

    Made function Parse which is dividing digits from symbols.

--- Laba3_Problem/Program.cs
+++ Laba3_Problem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Laba3_Problem
 {
@@ -17,7 +18,47 @@ namespace Laba3_Problem
                             problem += symbol;
                     }
                 }
-            Console.WriteLine(problem);
+
+            string[] result = Parse(problem);
+            foreach (var el in result)
+            {
+                Console.WriteLine(el);
+            }
+
         }
+
+        public static string[] Parse(string problem)
+        {
+            int pos1, pos2, i = 0;
+            List<string> result = new List<string>();
+            problem += '+';
+            while (i < problem.Length)
+            {
+                pos1 = i;
+                pos2 = pos1;
+                if (Char.IsDigit(problem[i]))
+                {
+                    for (int j = i + 1; j < problem.Length; j++)
+                    {
+                        if (Char.IsDigit(problem[j]))
+                            pos2 = j;
+                        else
+                        {
+                            result.Add(problem.Substring(pos1, pos2 - pos1 + 1));
+                            break;
+                        }
+
+                    }
+                }
+                else
+                    result.Add(Convert.ToString(problem[i]));
+
+                i = pos2 + 1;
+            }
+            result.RemoveAt(result.Count - 1);
+
+            return result.ToArray();
+        }
+        
     }
 }
