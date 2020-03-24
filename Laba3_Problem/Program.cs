446e2dbe8cab04fb5b02c0517da67af69fed534a
danil0110 <danilmail0110@gmail.com>
2020-03-24 15:25:58 +0200

    Added a problem solver function

--- Laba3_Problem/Program.cs
+++ Laba3_Problem/Program.cs
@@ -23,9 +23,51 @@ namespace Laba3_Problem
 
             string problem = "-23-2*9/4*2-11";
             string[] result = Parse(problem);
+            SolveProblem(result);
+        }
+
+        public static string[] Parse(string problem)
+        {
+            int pos1, pos2, i = 0;
+            List<string> result = new List<string>();
+            problem += '.';
+            while (i < problem.Length)
+            {
+                pos1 = i;
+                pos2 = pos1;
+                if (Char.IsDigit(problem[i]))
+                {
+                    for (int j = i + 1; j < problem.Length; j++)
+                    {
+                        if (Char.IsDigit(problem[j]))
+                            pos2 = j;
+                        else
+                        {
+                            result.Add(problem.Substring(pos1, pos2 - pos1 + 1));
+                            break;
+                        }
+
+                    }
+                }
+                else
+                    result.Add(Convert.ToString(problem[i]));
+
+                i = pos2 + 1;
+            }
+            result.RemoveAt(result.Count - 1);
+            if (result[0] == "-")
+            {
+                result.RemoveAt(0);
+                result[0] = "-" + result[0];
+            }
+            return result.ToArray();
+        }
+
+        public static void SolveProblem(string[] problem)
+        {
             MyStack<float> numbers = new MyStack<float>();
             MyStack<string> symbols = new MyStack<string>();
-            foreach (var el in result)
+            foreach (var el in problem)
             {
                 float number;
                 bool success = float.TryParse(el, out number);
@@ -71,44 +113,6 @@ namespace Laba3_Problem
                 Calculate(numbers, symbols);
             }
             Console.WriteLine($"Result: {numbers.Pop()}");
-            
-        }
-
-        public static string[] Parse(string problem)
-        {
-            int pos1, pos2, i = 0;
-            List<string> result = new List<string>();
-            problem += '.';
-            while (i < problem.Length)
-            {
-                pos1 = i;
-                pos2 = pos1;
-                if (Char.IsDigit(problem[i]))
-                {
-                    for (int j = i + 1; j < problem.Length; j++)
-                    {
-                        if (Char.IsDigit(problem[j]))
-                            pos2 = j;
-                        else
-                        {
-                            result.Add(problem.Substring(pos1, pos2 - pos1 + 1));
-                            break;
-                        }
-
-                    }
-                }
-                else
-                    result.Add(Convert.ToString(problem[i]));
-
-                i = pos2 + 1;
-            }
-            result.RemoveAt(result.Count - 1);
-            if (result[0] == "-")
-            {
-                result.RemoveAt(0);
-                result[0] = "-" + result[0];
-            }
-            return result.ToArray();
         }
 
         public static void Calculate(MyStack<float> numbers, MyStack<string> symbols)
